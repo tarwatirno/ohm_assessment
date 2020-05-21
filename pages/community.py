28b31a100897d5d48c672fea2e822293938943e3
Gregory Sizemore <tarwatirno@gmail.com>
2020-05-20 17:41:25 -0700

    Populate table with all users
    TODO: limit to most recent 5

--- pages/community.py
+++ pages/community.py
@@ -1,17 +1,19 @@
 from flask import jsonify, render_template, request, Response
 from flask.ext.login import current_user, login_user
 
+from flask_table import Table, Col
+
 from functions import app
 from models import User
 
+class CommunityTable(Table):
+    table_id = "community-table"
+    display_name = Col("Name")
+    tier = Col("Tier")
+    point_balance = Col("Point Balance")
 
 @app.route('/community', methods=['GET'])
 def community():
     login_user(User.query.get(1))
-
-    args = {
-            'gift_card_eligible': True,
-            'cashout_ok': True,
-            'user_below_silver': current_user.is_below_tier('Silver'),
-    }
-    return render_template("community.html", **args)
+    table = CommunityTable(User.query.all())
+    return render_template("community.html", table=table)
