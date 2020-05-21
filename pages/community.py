d75da6b52ea8d35d3098ae019176d9a7d2f97749
Gregory Sizemore <tarwatirno@gmail.com>
2020-05-20 17:59:12 -0700

    Limit and ordering for Community Table
    Also added zip column

--- pages/community.py
+++ pages/community.py
@@ -11,9 +11,10 @@ class CommunityTable(Table):
     display_name = Col("Name")
     tier = Col("Tier")
     point_balance = Col("Point Balance")
+    zip_code = Col("Zip")
 
 @app.route('/community', methods=['GET'])
 def community():
     login_user(User.query.get(1))
-    table = CommunityTable(User.query.all())
+    table = CommunityTable(User.query.order_by(User.create_time).limit(5))
     return render_template("community.html", table=table)
